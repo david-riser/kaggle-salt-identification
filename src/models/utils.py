6697ccdd35e13d70310789c865203c87e8d8a52f
David Riser <dmriser@gmail.com>
2018-08-11 18:58:13 -0400

    Broke the loading fn

--- src/models/utils.py
+++ src/models/utils.py
@@ -14,23 +14,38 @@
 import numpy as np
 import pandas as pd
 
-from keras.preprocessing.image import load_img
+from skimage.io import imread
 
 def load_data(path='../../data/raw/', image_width=128,
                        image_height=128, image_channels=1, sample_size=None):
     ''' This function is responsible for loading
     training images from the database. '''
 
-    train_ids, test_ids = find_indices()
-    depths = load_depths()
+    # The depth of all testing and training images.
+    depths = pd.read_csv(path + 'depths.csv', nrows=sample_size)
+    train = pd.read_csv(path + 'train.csv', nrows=sample_size)
 
-    train_df = construct_training_df(train_ids, depths)
-    test_df = construct_testing_df(test_ids, depths)
+    # Use depth information to find training and testing IDs
+    train_ids = train['id'].values
+    test_ids = [id for id in depths['id'].values if id not in train_ids]
 
-    train_df['image'] = load_train_images()
-    train_df['mask'] = load_train_masks()
-    test_df['image'] = load_test_images()
-    test_df['mask'] = load_test_masks()
+    train_images = [imread('{}train/images/{}.png'.format(path, id)) for id in train_ids]
+    train_masks = [imread('{}train/masks/{}.png'.format(path, id)) for id in train_ids]
+
+    test_images = [imread('{}test/images/{}.png'.format(path, id)) for id in test_ids]
+    test_masks = [imread('{}test/masks/{}.png'.format(path, id)) for id in test_ids]
+
+    train_df = pd.DataFrame({
+        'id':train_ids,
+        'image':train_images,
+        'mask':train_masks
+    })
+
+    test_df = pd.DataFrame({
+        'id':test_ids,
+        'image':test_images,
+        'mask':test_masks
+    })
 
     return train_df, test_df
 
