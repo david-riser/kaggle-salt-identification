ed4c45527107f2f0f07d0fd4fc9fc54d6b4069ca
David Riser <dmriser@gmail.com>
2018-08-11 16:54:30 -0400

    Added psuedocode to load data

--- src/models/utils.py
+++ src/models/utils.py
@@ -11,36 +11,31 @@
 # If the function is theirs, I have
 # given citation below.
 
-import glob
 import numpy as np
+import pandas as pd
 
-def load_training_data(path_to_train='../../data/raw/train/', image_width=128,
-                       image_height=128, image_channels=1, sample_size=500):
+from keras.preprocessing.image import load_img
+
+def load_data(path='../../data/raw/', image_width=128,
+                       image_height=128, image_channels=1, sample_size=None):
     ''' This function is responsible for loading
     training images from the database. '''
 
-    # Search the folder for png files.
-    image_names = glob.glob(path_to_train+'images/*.png')
-    n_images    = len(image_names)
-    print('Found %d images.' % n_images)
+    train_ids, test_ids = find_indices()
+    depths = load_depths()
 
+    train_df = construct_training_df(train_ids, depths)
+    test_df = construct_testing_df(test_ids, depths)
 
-    if sample_size is None:
-        sample_size = n_images
-    elif sample_size > n_images:
-        sample_size = n_images
+    train_df['image'] = load_train_images()
+    train_df['mask'] = load_train_masks()
+    test_df['image'] = load_test_images()
+    test_df['mask'] = load_test_masks()
 
-    print('Starting load of %d images' % sample_size)
-
-    x_train = np.zeros((sample_size, image_height, image_width, image_channels), dtype=np.uint8)
-    y_train = np.zeros((sample_size, image_height, image_width), dtype=np.bool)
-    for index, image_name in enumerate(image_names[:sample_size]):
-        image_id = image_name.split('.png')[0].split('/')[-1]
-
-        # Load the data here.
+    return train_df, test_df
 
 # Main method used only for testing purposes.
 if __name__ == '__main__':
-    load_training_data('../../data/raw/train/', sample_size=10)    # Simple test
-    load_training_data('../../data/raw/train/', sample_size=None)  # All loading test
-    load_training_data('../../data/raw/train/', sample_size=10000) # All loading test
+    load_data('../../data/raw/', sample_size=10)    # Simple test
+    load_data('../../data/raw/', sample_size=None)  # All loading test
+    load_data('../../data/raw/', sample_size=10000) # All loading test
